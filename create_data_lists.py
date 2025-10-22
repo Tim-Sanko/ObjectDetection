69bf71ac785a28efc71fd41a51f7146e289d6615
Tim-Sanko <timslendy@gmail.com>
2025-10-22 13:19:25 -0400

    initial commits

---
+++ create_data_lists.py
@@ -0,0 +1,7 @@
+from utils import create_data_lists
+
+if __name__ == '__main__':
+    print("starting...")
+    create_data_lists(voc07_path='./VOC2007',
+                      voc12_path='./VOC2012',
+                      output_folder='./')
